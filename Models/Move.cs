18a8e620c2aac12d7b4474249c6ae223b151ca30
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-27 20:00:15 +0100

    create class to act as type that the data is checked against

---
+++ Models/Move.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MoveTracker.Models
+{
+    [Table("move")]
+    public class Move
+    {
+        [PrimaryKey, AutoIncrement, Column("Id")]
+        public int Id { get; set; }
+        public int numOfMoves { get; set; }
+        public DateTime timeRecorded { get; set; }
+    }
+}
