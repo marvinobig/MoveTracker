ab56080562c03a27052301251f227682d8e553e1
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-29 08:14:49 +0100

    test update method

--- App.cs
+++ App.cs
@@ -8,5 +8,57 @@ namespace MoveTracker
 {
     internal class App
     {
+        private readonly string _dbPath;
+        private readonly int _seperator;
+        public App(string dbPath, int seperator)
+        {
+            _dbPath = dbPath;
+            _seperator = seperator;
+        }
+
+        public void DisplayIntro()
+        {
+            for (int i = 0; i < _seperator; i++)
+            {
+                Console.Write('-');
+            }
+
+            Console.WriteLine("Welcome to Move Tracker");
+            Console.WriteLine("Track how much you move throughout the day");
+
+            for (int i = 0; i < _seperator; i++)
+            {
+                Console.Write('-');
+            }
+        }
+
+        public void DisplayMenu() 
+        {
+            Console.Clear();
+
+            for (int i = 0; i < _seperator; i++)
+            {
+                Console.Write('-');
+            }
+
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("");
+            Console.WriteLine("Type 0 to Close Application");
+            Console.WriteLine("Type 1 to Insert a Record");
+            Console.WriteLine("Type 2 to View All Records");
+            Console.WriteLine("Type 3 to Update a Record");
+            Console.WriteLine("Type 4 to Delete a Record");
+
+            for (int i = 0; i < _seperator; i++)
+            {
+                Console.Write('-');
+            }
+
+            Console.WriteLine("");
+        }
+
+        static void QuitApp() => Environment.Exit(0);
     }
 }
